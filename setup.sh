e9075935ad88334bca90ccd30a95ced5d2d1673b
xeac <45033391+xeac@users.noreply.github.com>
2019-10-18 00:55:06 +0300

    Laravel Setup
    source https://www.digitalocean.com/community/tutorials/how-to-install-linux-nginx-mysql-php-lemp-stack-ubuntu-18-04

--- setup.sh
+++ setup.sh
@@ -1,1 +1,64 @@
+# Prerequisits:
+# - Allow inbound access to port 22 , 3389 and 80 (refer to repository NSG)
+
+sudo apt update
+sudo apt upgrade
+
+# set priviledges to user
+sudo-i
+usermod -aG sudo xeac
+
+# Setting Up a Basic Firewall
+sudo ufw app list
+
+# if OpenSSH not allowed run following:
+sudo ufw allow OpenSSH
+sudo ufw allow SSH
+sudo ufw enable
+
+# Installing the Nginx Web Server
+sudo apt install nginx
+sudo ufw allow 'Nginx HTTP'
+sudo ufw status
+# at least OpenSSH, SSH and Nginx HTTP should be active
+
+# Installing MySQL to Manage Site Data
+sudo apt install mysql-server
+sudo mysql_secure_installation
+
+# auth_socket plugin to access MySQL and root  - ask Marti
+
+# Installing PHP and Configuring Nginx to Use the PHP Processor
+
+sudo apt install php-fpm php-mysql
+sudo nano /etc/nginx/sites-available/naposao.rs
+
+server {
+        listen 80;
+        root /var/www/html;
+        index index.php index.html index.htm index.nginx-debian.html;
+        server_name 40.85.97.11;
+
+        location / {
+                try_files $uri $uri/ =404;
+        }
+
+        location ~ \.php$ {
+                include snippets/fastcgi-php.conf;
+                fastcgi_pass unix:/var/run/php/php7.2-fpm.sock;
+        }
+
+        location ~ /\.ht {
+                deny all;
+        }
+}
+
+sudo ln -s /etc/nginx/sites-available/naposao.rs /etc/nginx/sites-enabled/
+sudo unlink /etc/nginx/sites-enabled/default
+#check the config
+sudo nginx -t
+sudo systemctl reload nginx
+
+
+
 
